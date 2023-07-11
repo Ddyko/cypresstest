f289e33b9ca501672a719a7d467d3495835a5800
Ddyko <andiko.gunawan@gmail.com>
2023-07-11 17:46:55 +0700

    added email and password input

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,6 +2,12 @@ describe('User Login using the correct credentials', () => {
   it('Given user is already in login page', () => {
     cy.visit('https://secondhand-store.herokuapp.com/')
 
-    cy.contains('Masuk').click()
+    cy.contains('Masuk').click(),
+
+    cy.get('#exampleInputEmail1').type('andiko.gunawan@gmail.com'),
+
+    cy.get('#exampleInputPassword1').type('12345678')
+
+
   })
 })
