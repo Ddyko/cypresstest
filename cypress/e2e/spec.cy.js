ffaa53a14b1daad41e0b82827c0a607e90b62ca7
Ddyko <andiko.gunawan@gmail.com>
2023-07-12 18:24:15 +0700

    tring to add more steps

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,12 +2,17 @@ describe('User Login using the correct credentials', () => {
   it('Given user is already in login page', () => {
     cy.visit('https://secondhand-store.herokuapp.com/')
 
-    cy.contains('Masuk').click(),
-
-    cy.get('#exampleInputEmail1').type('andiko.gunawan@gmail.com'),
-
-    cy.get('#exampleInputPassword1').type('12345678')
-
+    cy.contains('Masuk').click()
 
   })
+
+  it('When user inpur correct credential and press Masuk button', () => {
+    
+    cy.get('#exampleInputEmail1').type('andiko.gunawan@gmail.com'),
+
+    cy.get('input[id="exampleInputPassword1 "]').type('12345678')
+
+    cy.get('button').click()
+
+  });
 })
