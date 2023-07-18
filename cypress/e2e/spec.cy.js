737a8b3e9bb74e5545ef95db02308561edfc6ee6
Ddyko <andiko.gunawan@gmail.com>
2023-07-18 18:57:16 +0700

    fixed cypress config and added validation on 1st step

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,22 @@
+/// <reference types="cypress" />
+
 describe('User Login using the correct credentials', () => {
   it('Given user is already in login page', () => {
     cy.visit('https://secondhand-store.herokuapp.com/')
 
     cy.contains('Masuk').click()
 
-  })
+    cy.get('strong').should('have.text', 'Masuk')
 
-  it('When user inpur correct credential and press Masuk button', () => {
+    cy.get('#exampleInputEmail1').should('not.contain.text')
+
+    cy.get('input[id="exampleInputPassword1 "]').should('not.contain.text')
+
+  })  
+
+  it('When user input correct credential and press Masuk button', () => {
     
-    cy.get('#exampleInputEmail1').type('andiko.gunawan@gmail.com'),
+    cy.get('#exampleInputEmail1', {timeout: 6000}).type('andiko.gunawan@gmail.com'),
 
     cy.get('input[id="exampleInputPassword1 "]').type('12345678')
 
