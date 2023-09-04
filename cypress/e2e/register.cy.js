fbd881f2bcc94ebc6d6aeef838b8f79c01310d41
Ddyko <andiko.gunawan@gmail.com>
2023-09-04 09:57:56 +0700

    added invalid email format case on register case

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -58,7 +58,7 @@ describe('User want to register an account using an already registered email', (
       cy.get('#inputAddress').type(userID_Alpha())
   })
 
-  it('And user input unregistered email', () => {
+  it('And user input registered email', () => {
       cy.get('#exampleInputEmail1').type('andiko.gunawan@gmail.com')
   })
 
@@ -70,4 +70,27 @@ describe('User want to register an account using an already registered email', (
   it('Then user gets email already registered alert', () => {
       cy.get('.alert > strong').should('contain.text','Email sudah digunakan')
   })
+})
+
+describe('User want to register using an invalid format email', () => {
+    it('Given user already in register page', () => {
+        allPage.openRegister()
+        allPage.validRegisterPage()
+    })
+    it('When user input name', () => {
+        cy.get('#inputAddress').type(userID_Alpha())
+    })
+
+    it('User input invalid format email', () => {
+        cy.get('#exampleInputEmail1').type('.com@@')
+    })
+
+    it('And user input correct format password and pressed the daftar button', () => {
+        allPage.inputPassword()
+        cy.get('.btn').click()
+    })
+
+    it('Then user will stay in register page', () => {
+        cy.get('strong').should('have.text', 'Daftar')
+    })
 })
